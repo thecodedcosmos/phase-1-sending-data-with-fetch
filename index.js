4c182c97e4ddfe41fc619e85806571f787bebf5f
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-10-22 06:15:44 -0700

    Complete POST request

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name,email){
+    const formData = {
+        name: `${name}`,
+        email: `${email}`,
+    };
+    fetch(`http://localhost:3000/users`,
+    {method: "Post",  headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },body: JSON.stringify(formData)
+    })
+    
+    .then(response=>response.json())
+
+    .then((data)=>{document.body.innerHTML= data.id})
+
+    .catch(err=>document.body.innerHTML = err.message
+)}
